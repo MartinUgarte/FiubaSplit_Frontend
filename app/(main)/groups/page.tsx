dc1e61a385c41048dd4ec4490b30956190551228
DiegoCivi <dcivini@fi.uba.ar>
2024-05-16 10:50:39 -0300

    Add handele for error when creating group with a name that is already used

--- app/(main)/groups/page.tsx
+++ app/(main)/groups/page.tsx
@@ -1,78 +1,95 @@
 "use client";
 
-import { useEffect, useState } from 'react'
-import GroupCard from './GroupCard'
-import { Box, Button, Grid } from '@mui/material'
-import { Group, dumpGroup } from '@/app/types';
-import CreateGroupModal from './CreateGroupModal';
+import { useEffect, useState } from "react";
+import GroupCard from "./GroupCard";
+import { Box, Button, Grid } from "@mui/material";
+import { Group, dumpGroup } from "@/app/types";
+import CreateGroupModal from "./CreateGroupModal";
 
 type FormValues = {
-    name: string;
-    description: string;
-}
+  name: string;
+  description: string;
+};
 
 export default function GroupsHome() {
-    const [groups, setGroups] = useState<Group[]>([dumpGroup]);
-    const [showCreateGroupModal, setShowCreateGroupModal] = useState(false);
+  const [groups, setGroups] = useState<Group[]>([dumpGroup]);
+  const [showCreateGroupModal, setShowCreateGroupModal] = useState(false);
 
-    useEffect(() => {
-        getGroups()
-    }, [])
+  useEffect(() => {
+    getGroups();
+  }, []);
 
-    const getGroups = () => {
-        const jwt = localStorage.getItem('jwtToken');
-        fetch(`http://localhost:8000/groups`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${jwt}`
-            }
-        })
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return res.json()
-            })
-            .then((data) => {
-                console.log("Got groups: ", data)
-                setGroups(data.map((group: Group) => {
-                    return {
-                        id: group.id,
-                        name: group.name,
-                        description: group.description,
-                        creator_id: group.creator_id,
-                        category: group.category
-                    }
-                }))
-            })
-
+  const checkMember = (group: Group) => {
+    const userId = localStorage.getItem("userId");
+    console.log("GROUP: ", group);
+    console.log("ID del user: ", userId);
+    if (userId) {
+      return group.members.includes(userId);
     }
+    console.log("El item del local storage userId es null");
+    return false;
+  };
 
-    return (
-        <Box display='flex' flex='1' flexDirection='column'>
-            <CreateGroupModal open={showCreateGroupModal} onClose={() => setShowCreateGroupModal(false)} getGroups={() => getGroups()} />
-            <Box display='flex' flex='0.1' justifyContent='flex-end'>
-                <Button
-                    variant="outlined"
-                    sx={{ height: 40 }}
-                    onClick={() => setShowCreateGroupModal(true)}
-                >
-                    Create group
-                </Button>
-            </Box>
-            <Box display='flex' flex='0.9'>
-                <Grid container spacing={5} sx={{ marginTop: 1 }}>
-                    {groups.map((group) => (
-                        <Grid item xs={6} key={group.id}>
-                            <GroupCard
-                                group={group}
-                                getGroups={() => getGroups()}
-                            />
-                        </Grid>
-                    ))}
+  const getGroups = () => {
+    const jwt = localStorage.getItem("jwtToken");
+    fetch(`http://localhost:8000/groups`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwt}`,
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log("Got groups: ", data);
+        setGroups(
+          data.map((group: Group) => {
+            return {
+              id: group.id,
+              name: group.name,
+              description: group.description,
+              creator_id: group.creator_id,
+              category: group.category,
+              members: group.members,
+            };
+          })
+        );
+      });
+  };
+
+  return (
+    <Box display="flex" flex="1" flexDirection="column">
+      <CreateGroupModal
+        open={showCreateGroupModal}
+        onClose={() => setShowCreateGroupModal(false)}
+        getGroups={() => getGroups()}
+      />
+      <Box display="flex" flex="0.1" justifyContent="flex-end">
+        <Button
+          variant="outlined"
+          sx={{ height: 40 }}
+          onClick={() => setShowCreateGroupModal(true)}
+        >
+          Create group
+        </Button>
+      </Box>
+      <Box display="flex" flex="0.9">
+        <Grid container spacing={5} sx={{ marginTop: 1 }}>
+          {groups.map(
+            (group) =>
+              checkMember(group) && (
+                <Grid item xs={6} key={group.id}>
+                  <GroupCard group={group} getGroups={() => getGroups()} />
                 </Grid>
-            </Box>
-        </Box>
-    )
+              )
+          )}
+        </Grid>
+      </Box>
+    </Box>
+  );
 }
